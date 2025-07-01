740d468c1ca37d32aad3822ac00892e2df3491df
Eugene Belkovich <eugene.belkovich@gmail.com>
2025-07-01 11:57:04 -0300

    feat: redirect by shortUrl(slug)implemented

--- src/modules/url/url.controller.ts
+++ src/modules/url/url.controller.ts
@@ -1,7 +1,11 @@
-import {Controller, Get, Post, Body, Logger, Param} from '@nestjs/common';
+import {Controller, Get, Post, Body, Logger, Param, Res, Req, HttpStatus} from '@nestjs/common';
+import {FastifyReply} from 'fastify';
+import {isEmpty} from 'lodash';
+
 import {UrlService} from './url.service';
 import {CreateUrlDto} from './dto/create-url.dto';
 import {UrlListResponseDto, UrlResponseDto} from '@/modules/url/dto/url-response.dto';
+import {GetUrlBySlugParams} from '@/modules/url/dto/get-url-by-slug.dto';
 
 @Controller('urls')
 export class UrlController {
@@ -16,9 +20,28 @@ export class UrlController {
   }
 
   @Get(':slug')
-  async getUrlBySlug(@Param('slug') slug: string): Promise<UrlResponseDto | null> {
-    this.logger.log(`Getting URLs list`);
-    return await this.urlService.getUrlBySlug(slug);
+  async redirectToOriginalUrl(@Param() param: GetUrlBySlugParams, @Res() res: FastifyReply): Promise<void> {
+    this.logger.log(`Redirecting slug: ${param.slug}`);
+
+    const urlRecord = await this.urlService.getUrlBySlug(param.slug);
+
+    if (!urlRecord || isEmpty(urlRecord.originalUrl)) {
+      this.logger.warn(`URL not found for slug: ${param.slug}`);
+      res.status(HttpStatus.NOT_FOUND).send({
+        statusCode: HttpStatus.NOT_FOUND,
+        message: 'URL not found',
+        error: 'Not Found'
+      });
+      return;
+    }
+
+    res.headers({
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      Pragma: 'no-cache',
+      Expires: '0'
+    });
+
+    res.redirect(urlRecord.originalUrl, HttpStatus.FOUND);
   }
 
   @Get()
